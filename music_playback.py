195b631cfcba109bb8ebd3d679888d56d382e07e
rephaelberkooz <rephael@berkooz.com>
2023-10-22 21:24:29 -0400

    reorg and multithreading

--- music_playback.py
+++ music_playback.py
@@ -7,12 +7,40 @@ import pickle
 import struct
 import sounddevice as sd
 import soundfile as sf
+import queue
 
 # host_ip = socket.gethostbyname(socket.gethostname())
 host_ip = "127.0.0.1"
 port = 8080
 
-def start_stream():
+class Playback_Thread(threading.Thread):
+    def __init__(self, file_name):
+        super().__init__()
+        self.coordinates_queue = queue.Queue()
+        self.file_name = file_name
+
+        wf = wave.open(file_name, 'rb')
+        framerate = 48000
+
+        chunck_size = 1024
+
+        p = pyaudio.PyAudio()
+        stream = p.open(format=p.get_format_from_width(2),
+                    channels=2,
+                    rate=framerate,
+                    output=True,
+                    frames_per_buffer=chunck_size)
+        
+        while True:
+            packet = wf.readframes(chunck_size)
+            
+            if not self.coordinates_queue.empty():
+                print("message: ", self.coordinates_queue.get())
+               
+
+            stream.write(packet)
+
+def recieve_stream():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_address = (host_ip, port)
     server_socket.bind(socket_address)
@@ -22,22 +50,36 @@ def start_stream():
     client_socket, client_address = server_socket.accept() 
     print(f"Accepted connection from: {client_address}")
 
+    # Assuming the first packet contains the filename for music playback
     file_path = client_socket.recv(1024).decode("utf-8")
-    play_wav(file_path=file_path, duration_s=5)
+    # play_wav(file_path=file_path, duration_s=5)
+    wf = wave.open(file_path, 'rb')
+    framerate = 48000
+
+    chunck_size = 1024
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(2),
+                channels=2,
+                rate=48000,
+                output=True,
+                frames_per_buffer=chunck_size)
+    
     while True:
         data = client_socket.recv(1024).decode("utf-8")
+        packet = wf.readframes(chunck_size)
+        stream.write(packet)
+        
         if not data:
             break  # Break the loop if the connection is closed
         print(f"Received: {data}")
 
-    # file_path = "dreams.wav"
 
     # Close the client and server sockets
     client_socket.close()
     server_socket.close()
 
 
-# C:\\Users\\Sam\\Documents\\Beat Saber\\Beat Saber\\Assets\\Beat Saber_Regular Intervals(slow)_Aespa Dreams Come True_PANNED.wav
 def play_wav(file_path, duration_s):
     try:
         # Read the WAV file
@@ -52,13 +94,23 @@ def play_wav(file_path, duration_s):
 
         # Play the audio data
         sd.play(audio_data_to_play, sample_rate)
-        sd.wait()  # Wait for the playback to finish
+        # sd.wait()  # Wait for the playback to finish
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
+# def play_wav_chunks(file_name):
 
 
-t1 = threading.Thread(target=start_stream, args=())
-t1.start()
-# play_wav("dreams.wav", 5)
+
+    
+
+if __name__ == "__main__":
+    playback_thread = Playback_Thread("ln_Savanne.wav")
+    playback_thread.start()
+    playback_thread.join()
+
+    # t1 = threading.Thread(target=recieve_stream, args=())
+    # t1.start()
+    # play_wav("dreams.wav", 5)
+    # t1.join()
---
+++ simulate_unity_tcp.py
@@ -0,0 +1,44 @@
+import socket
+import os
+import threading
+import pyaudio
+import pickle
+import struct
+import random
+import time
+
+num_blocks = 10
+
+# get the host's name and IP address
+# host_ip = socket.gethostbyname(socket.gethostname())
+host_ip = "127.0.0.1"
+port = 8080
+
+file_path = "ln_Savanne.wav"
+
+def send_message_tcp(socket, data):
+    message = data.encode()
+    # serialized_data = pickle.dumps(data)
+    # message = struct.pack("Q", len(serialized_data)) + serialized_data
+    socket.sendall(message)
+
+# create socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_address = (host_ip, port)
+print('server listening at', socket_address)
+client_socket.connect(socket_address) 
+print("CLIENT CONNECTED TO", socket_address)
+
+payload_size = struct.calcsize("Q")
+
+
+send_message_tcp(client_socket, file_path)
+
+
+for _ in range(num_blocks):
+    coordinates = f'{[random.random()*20, random.random()*20, 15]}'
+
+    time.sleep(1)
+    send_message_tcp(client_socket, coordinates)
+
+client_socket.close()
